df153df926e40626fab2277f4238e2ad6af74d94
w3schoolsua <57772958+w3schoolsua@users.noreply.github.com>
2021-01-26 17:34:56 +0200

    Add files via upload

--- scripts/sqltryit_end.js
+++ scripts/sqltryit_end.js
@@ -13,7 +13,6 @@
 //  window.editor.on("change", function () {window.editor.save();});
 }
     colorcoding();
-
     var w3schoolsWebSQLOK = !!window.openDatabase;
     var statementSupport = document.getElementById("sSupport").value;
     if (w3schoolsWebSQLOK === true && statementSupport === "") {
@@ -22,13 +21,11 @@
     var ifr = document.getElementById("iframeResultSQL");
     var idoc = (ifr.contentWindow || ifr.contentDocument);
     if (idoc.document) idoc=idoc.document;
-    idoc.write("<div style='margin:10px;font-family:verdana;font-size:12px;'>Нажмите <b>'Выполнить SQL'</b> для выполнения SQL-инструкции выше.</div>");
+    idoc.write("<div style='margin:10px;font-size:14px;font-family: Verdana, sans-serif'>Натисніть <b>'Виконати SQL'</b> для виконання SQL-інструкції вище.</div>");
 }
-
     function showDescription() {
     document.getElementById("descriptionDIV").style.display = "block";
 }
-
     function hideDescription() {
     document.getElementById("descriptionDIV").style.display = "none";
 }
@@ -46,7 +43,6 @@
     w3schoolsNoWebSQLSubmit();
 }
 }
-
     function w3schoolsWriteDBInfo() {
     var txt;
     if (w3schoolsWebSQLOK === true && statementSupport === "") {
@@ -74,48 +70,42 @@
     document.getElementById("divResultSQL").style.display="none";
     document.getElementById("iframeResultSQL").style.display="inline";
     txt = '';
-    txt = txt + '<table width="100%" class="notranslate">';
+    txt = txt + '<table style="width: 100%" class="notranslate">';
     txt = txt + '<tr><th style="text-align:left;">Названия таблиц</th><th style="text-align:right;">Записи</th></tr>';
-    txt = txt + '<tr><td style="text-align:left;cursor:pointer;text-decoration:underline;" onclick="w3schoolsNoWebSQLSelectStar(\'Customers\')">Customers</td>';
+    txt = txt + '<tr><td style="text-align:left;cursor:pointer;text-decoration:underline;" onclick="w3schoolsNoWebSQLSelectStar(\'Customers\')">Клієнти</td>';
     txt = txt + '<td style="text-align:right;">91</td>';
-    txt = txt + '</tr><tr><td style="text-align:left;cursor:pointer;text-decoration:underline;" onclick="w3schoolsNoWebSQLSelectStar(\'Categories\')">Categories</td>';
+    txt = txt + '</tr><tr><td style="text-align:left;cursor:pointer;text-decoration:underline;" onclick="w3schoolsNoWebSQLSelectStar(\'Categories\')">Категорії</td>';
     txt = txt + '<td style="text-align:right;">8</td>';
-    txt = txt + '</tr><tr><td style="text-align:left;cursor:pointer;text-decoration:underline;" onclick="w3schoolsNoWebSQLSelectStar(\'Employees\')">Employees</td>';
+    txt = txt + '</tr><tr><td style="text-align:left;cursor:pointer;text-decoration:underline;" onclick="w3schoolsNoWebSQLSelectStar(\'Employees\')">Співробітники </td>';
     txt = txt + '<td style="text-align:right;">10</td></tr>';
-    txt = txt + '<tr><td style="text-align:left;cursor:pointer;text-decoration:underline;" onclick="w3schoolsNoWebSQLSelectStar(\'OrderDetails\')">OrderDetails</td>';
+    txt = txt + '<tr><td style="text-align:left;cursor:pointer;text-decoration:underline;" onclick="w3schoolsNoWebSQLSelectStar(\'OrderDetails\')">Деталі замовлення</td>';
     txt = txt + '<td style="text-align:right;">518</td></tr>';
-    txt = txt + '<tr><td style="text-align:left;cursor:pointer;text-decoration:underline;" onclick="w3schoolsNoWebSQLSelectStar(\'Orders\')">Orders</td>';
+    txt = txt + '<tr><td style="text-align:left;cursor:pointer;text-decoration:underline;" onclick="w3schoolsNoWebSQLSelectStar(\'Orders\')">Замовлення</td>';
     txt = txt + '<td style="text-align:right;">196</td></tr>';
-    txt = txt + '<tr><td style="text-align:left;cursor:pointer;text-decoration:underline;" onclick="w3schoolsNoWebSQLSelectStar(\'Products\')">Products</td>';
+    txt = txt + '<tr><td style="text-align:left;cursor:pointer;text-decoration:underline;" onclick="w3schoolsNoWebSQLSelectStar(\'Products\')">Продукти</td>';
     txt = txt + '<td style="text-align:right;">77</td></tr>';
-    txt = txt + '<tr><td style="text-align:left;cursor:pointer;text-decoration:underline;" onclick="w3schoolsNoWebSQLSelectStar(\'Shippers\')">Shippers</td>';
+    txt = txt + '<tr><td style="text-align:left;cursor:pointer;text-decoration:underline;" onclick="w3schoolsNoWebSQLSelectStar(\'Shippers\')">Вантажовідправники</td>';
     txt = txt + '<td style="text-align:right;">3</td></tr>';
-    txt = txt + '<tr><td style="text-align:left;cursor:pointer;text-decoration:underline;" onclick="w3schoolsNoWebSQLSelectStar(\'Suppliers\')">Suppliers</td>';
+    txt = txt + '<tr><td style="text-align:left;cursor:pointer;text-decoration:underline;" onclick="w3schoolsNoWebSQLSelectStar(\'Suppliers\')">Постачальники</td>';
     txt = txt + '<td style="text-align:right;">29</td></tr>';
     txt = txt + '</table>';
     document.getElementById("yourDB").innerHTML = txt;
 }
 }
-
     function w3schoolsNoWebSQLSelectStar(x) {
     var sql = "SELECT * FROM " + x + ";";
     document.getElementById("textareaCodeSQL").value = sql;
     window.editor.getDoc().setValue(sql);
     w3schoolsNoWebSQLSubmit();
 }
-
     function w3schoolsNoWebSQLSubmit() {
     var t = document.getElementById("textareaCodeSQL").value;
     document.getElementById("code").value = t;
-
     t=escape(t);document.getElementById("bt").value="1";
-
     document.getElementById("tryitform").action="https://tryit.w3schools.com/trysql_view.asp?x=" + Math.random();
-
     validateForm();
     document.getElementById("tryitform").submit();
 }
-
     function validateForm() {
     var code=document.getElementById("code").value;
     if (code.length>5000) {
