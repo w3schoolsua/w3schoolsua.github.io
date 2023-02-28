20a576ead348797ffcff7d3dcdeaaffa29b3874c
w3schoolsua <w3schoolsua@gmail.com>
2023-03-01 01:53:07 +0200

    update
    Signed-off-by: w3schoolsua <w3schoolsua@gmail.com>

---
+++ php/poll_vote.php
@@ -0,0 +1,21 @@
+<h2>Result:</h2>
+<table>
+    <tr>
+        <td>Yes:</td>
+        <td>
+            <img src="poll.gif"
+                 width='98'
+                 height='20'>
+            98%
+        </td>
+    </tr>
+    <tr>
+        <td>No:</td>
+        <td>
+            <img src="poll.gif"
+                 width='2'
+                 height='20'>
+            2%
+        </td>
+    </tr>
+</table>
