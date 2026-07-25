cfea5f4ebe0d4a7a2963bada160cc50bd2ae594a
w3schoolsua <57772958+w3schoolsua@users.noreply.github.com>
2026-07-25 20:13:57 +0300

    updated
    Signed-off-by: w3schoolsua <57772958+w3schoolsua@users.noreply.github.com>

--- scripts/current-year.js
+++ scripts/current-year.js
@@ -1,4 +1,23 @@
-<!--Автоматичне відображення поточного року-->
-    document.addEventListener("DOMContentLoaded", function () {
+// Автоматичне відображення поточного року
+document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("current-year").textContent = new Date().getFullYear();
+});
+// Дата оновлення сторінки
+document.addEventListener("DOMContentLoaded", () => {
+  const el = document.getElementById("updated-time");
+
+  const date = new Date(document.lastModified);
+
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = date.getFullYear();
+
+  // Формат для атрибуту datetime: рррр-мм-дд
+  const datetimeValue = `${year}-${month}-${day}`;
+
+  // Формат для відображення: дд.мм.рррр
+  const displayValue = `${day}.${month}.${year}`;
+
+  el.setAttribute("datetime", datetimeValue);
+  el.textContent = displayValue;
 });
